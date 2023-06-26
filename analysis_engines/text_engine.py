2f707be03a637335764954b610c958377cd77bb1
Atang F Mokamogo <atangfmokamogo@outlook.com>
2023-06-26 21:37:14 +0200

    fixed security flaw: Leaked api_access token

--- analysis_engines/text_engine.py
+++ analysis_engines/text_engine.py
@@ -9,7 +9,7 @@ import time
 class TextEngine():
     
     def __init__(self):
-        self.__ACCESS_TOKEN = "02eSslKwFRZ7M3hO3B-mtupMCN2KpBTpbPi6QKZgmIYLhg1nZZ7pBA6Dc1j83L0IgMtRpWr_YNy4vycW1RKDWTgOfRGF4"
+        self.__ACCESS_TOKEN = 
         
         
     def _request_state(self, query_id):
